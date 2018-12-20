83995569dee918d423c7fc61c5e10e325dce7228
kevintoddhawkins <kevintoddhawkins@gmail.com>
2018-12-20 07:35:22 -0600

    Initial Project Set-Up / Hello, World

---
+++ kanbanConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kanbanConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Quick Check to make sure posting to GitHub works
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+
+        }
+    }
+}
